3705910dff9ad2bd6187c02f37403849d6f9ab37
Joel <standevcode@gmail.com>
2023-11-11 23:01:52 +0100

    pulled updates from remote origin

--- lib/entities/team.dart
+++ lib/entities/team.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// import 'package:objectbox/objectbox.dart';
->>>>>>> 6278e5815fd23005ad0a965bc539bd3c480b5db7
 
 // import '../objectbox.g.dart';
 // import 'workspace.dart';
